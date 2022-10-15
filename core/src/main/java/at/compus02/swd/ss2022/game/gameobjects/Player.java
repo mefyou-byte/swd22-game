4979ba99a601211b67d290b5fa5fc8574fc44022
Westreicher Max <Max.Westreicher@ksengineers.at>
2022-10-15 12:45:09 +0200

    Player model added

---
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/Player.java
@@ -0,0 +1,48 @@
+package at.compus02.swd.ss2022.game.gameobjects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Player implements GameObject {
+    private Texture image;
+    private Sprite sprite;
+
+    public Player() {
+        image = new Texture("Player.png");
+        sprite = new Sprite(image);
+    }
+    @Override
+    public void act(float delta) {
+
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        sprite.setPosition(x, y);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+
+    public void moveUp()
+    {
+        sprite.setPosition(sprite.getX(), sprite.getY()+10);
+    }
+
+    public void moveDown()
+    {
+        sprite.setPosition(sprite.getX(), sprite.getY()-10);
+    }
+    public void moveRight()
+    {
+        sprite.setPosition(sprite.getX()+10, sprite.getY());
+    }
+    public void moveLeft()
+    {
+        sprite.setPosition(sprite.getX()-10, sprite.getY());
+    }
+
+}
